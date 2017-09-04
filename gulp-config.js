a1bb792ddde7c2dc20bb3b287bbe3b887b638069
M. Nowak <mnowak+git@gmail.com>
2017-09-03 22:49:51 -0400

    Transition and headers and block areas

--- gulp-config.js
+++ gulp-config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
 
     {
-      src : "https://eonewebsite.imgix.net/cmsasset/feature/6260921280657096704/385a51264606.jpg",
+      src : "https://eonewebsite-dev.imgix.net/dev/cmsasset/feature/6242020667298938880/a0528684f4d3.jpg",
       class : "double",
       url : "second",
       title : "Blasters of the Universe",
@@ -32,14 +32,20 @@ module.exports = {
 
     },
     {
-      src : "https://eonewebsite.imgix.net/cmsasset/feature/6260921280657096704/385a51264606.jpg",
+      src : "https://eonewebsite.imgix.net/cmsasset/feature/6274957229472874496/51e10da4a468.jpg",
       class : "double",
       url : "third",
       title : "Blasters of the Universe",
       description : "Description"
 
     },
+    {
+      class : "text",
+      url : "third",
+      title : "Blasters of the Universe",
+      description : "Description"
 
+    },
     {
       src : "https://eonewebsite.imgix.net/cmsasset/feature/6260921280657096704/385a51264606.jpg",
       class : "single",
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,6 +24,9 @@ var verge = require('./vendor/verge.min.js');
 
 
 	let getCurrent = function() {
+
+		let gtop = document.getElementsByClassName("gallery")[0].offsetTop;
+
 		for (let [index, item] of gridItems.entries()) {
 			//console.log (item, );
 			let last = item.visible;
@@ -31,18 +34,24 @@ var verge = require('./vendor/verge.min.js');
 			if (current != last) {
 				console.log ("CHANGE", gridItems[index]);
 				gridItems[index].visible = current;
+
+				// if (!current) {
+				// 	item.dom.classList.remove('active', current);
+				// }
 			}
 			if (current == true && last == true) {
 				console.log(">> ", item.dom.offsetTop, lastScrollPos, verge.viewportH());
-				let diff = Math.abs( ( lastScrollPos - item.dom.offsetTop ) / verge.viewportH() );
+				let diff = Math.abs( ( lastScrollPos + gtop/2 - item.dom.offsetTop) / verge.viewportH() );
 				diff = diff<.5 ? 2*diff*diff : -1+(4-2*diff)*diff;
 
 				console.log(Math.abs(diff));
 
 				item.dom.style.opacity = 1 - Math.abs(diff);
 
-				if (diff > 0) {
-					//item.dom.style.transform = scale(0.5 + 0.5*Math.abs(diff), 0.5 + 0.5*Math.abs(diff))
+				if (1 - diff > 0.65) {
+					item.dom.classList.add('active');
+				} else {
+					item.dom.classList.remove('active');
 				}
 
 			}
@@ -59,8 +68,8 @@ var verge = require('./vendor/verge.min.js');
 
 		let _items = document.getElementsByClassName("gallery__grid__item");
 		for (let _item of _items) {
-			let isVis = verge.inViewport(_item);
-			gridItems.push({dom:_item, visible: isVis, active: false});
+			//let isVis = verge.inViewport(_item);
+			gridItems.push({dom:_item, visible: false, active: false});
 		}
 
 		getCurrent();
